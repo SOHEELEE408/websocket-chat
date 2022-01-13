b150efb740aaf57ac6132385fce95de95d44b025
minseok <minseok7407@naver.com>
2022-01-13 17:41:53 +0900

    stomp 사용을 위한 의존성 추가, application.properties 대신 yml 생성, 채팅방 생성, 조회 Repository 생성 및 Service 삭제, Handler 삭제

--- src/main/java/com/websocket/chat/domain/room/ChatRoom.java
+++ src/main/java/com/websocket/chat/domain/room/ChatRoom.java
@@ -1,35 +1,40 @@
 package com.websocket.chat.domain.room;
 
 import com.websocket.chat.domain.message.ChatMessage;
-import com.websocket.chat.service.ChatService;
+
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 @Getter
+@Setter
 public class ChatRoom {
     private String roomId;
     private String name;
-    private final Set<WebSocketSession> sessions = new HashSet<>();
+//    private final Set<WebSocketSession> sessions = new HashSet<>();
 
-    @Builder
-    public ChatRoom(String roodId, String name){
-        this.roomId=roodId;
-        this.name=name;
+
+    public static ChatRoom create(String name){
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name=name;
+        return chatRoom;
     }
 
-    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService){
-        if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)){
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender()+"님이 입장하셨습니다.");
-        }
-        sendMessage(chatMessage, chatService);
-    }
-
-    public <T> void sendMessage(T message, ChatService chatService){
-        sessions.parallelStream().forEach(session->chatService.sendMessage(session, message));
-    }
+//    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService){
+//        if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)){
+//            sessions.add(session);
+//            chatMessage.setMessage(chatMessage.getSender()+"님이 입장하셨습니다.");
+//        }
+//        sendMessage(chatMessage, chatService);
+//    }
+//
+//    public <T> void sendMessage(T message, ChatService chatService){
+//        sessions.parallelStream().forEach(session->chatService.sendMessage(session, message));
+//    }
 }
--- src/main/java/com/websocket/chat/handler/WebSockChatHandler.java
+++ src/main/java/com/websocket/chat/handler/WebSockChatHandler.java
@@ -1,32 +1,33 @@
-package com.websocket.chat.handler;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.websocket.chat.domain.message.ChatMessage;
-import com.websocket.chat.domain.room.ChatRoom;
-import com.websocket.chat.service.ChatService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-
-@Slf4j
-@RequiredArgsConstructor
-@Component
-public class WebSockChatHandler extends TextWebSocketHandler {
-    private final ObjectMapper objectMapper;
-    private final ChatService chatService;
-
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String payload = message.getPayload();
-        log.info("payload {}", payload);
-//        TextMessage textMessage = new TextMessage("Welcome chatting server");
-//        session.sendMessage(textMessage);
-        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
-        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
-        room.handleActions(session, chatMessage, chatService);
-    }
-}
+//package com.websocket.chat.handler;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.websocket.chat.domain.message.ChatMessage;
+//import com.websocket.chat.domain.room.ChatRoom;
+//import com.websocket.chat.domain.room.ChatRoomRepository;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@Component
+//public class WebSockChatHandler extends TextWebSocketHandler {
+//    private final ObjectMapper objectMapper;
+//    private final ChatRoomRepository chatRoomRepository;
+//
+//    @Override
+//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+//        String payload = message.getPayload();
+//        log.info("payload {}", payload);
+////        TextMessage textMessage = new TextMessage("Welcome chatting server");
+////        session.sendMessage(textMessage);
+//        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+//        ChatRoom room = chatRoomRepository.findRoomById(chatMessage.getRoomId());
+////        room.handleActions(session, chatMessage, chatRoomRepository);
+//    }
+//}
