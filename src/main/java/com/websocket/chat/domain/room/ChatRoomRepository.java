06fe58340655d4316675a81ddc50624e201a8c59
minseok <minseok7407@naver.com>
2022-01-14 18:18:07 +0900

    redis를 저장소로 이용하여 pub/sub 구현

--- src/main/java/com/websocket/chat/domain/room/ChatRoomRepository.java
+++ src/main/java/com/websocket/chat/domain/room/ChatRoomRepository.java
@@ -1,33 +1,66 @@
 package com.websocket.chat.domain.room;
 
+import com.websocket.chat.service.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
 
+@RequiredArgsConstructor
 @Repository
 public class ChatRoomRepository {
 
-    private Map<String, ChatRoom> chatRoomMap;
+    private final RedisMessageListenerContainer redisMessageListenerContainer;
+    private final RedisSubscriber redisSubscriber;
+
+    private static String CHAT_ROOMS="CHAT_ROOM";
+    private final RedisTemplate<String, Object> redisTemplate;
+    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+
+    private Map<String, ChannelTopic> topics;
+//    private Map<String, ChatRoom> chatRoomMap;
 
     @PostConstruct
     private void init(){
-        chatRoomMap = new LinkedHashMap<>();
+        opsHashChatRoom = redisTemplate.opsForHash();
+        topics = new HashMap<>();
+//        chatRoomMap = new LinkedHashMap<>();
     }
 
     public List<ChatRoom> findAllRoom(){
-        List chatRooms = new ArrayList<>(chatRoomMap.values());
-        Collections.reverse(chatRooms);
-        return chatRooms;
+        return opsHashChatRoom.values(CHAT_ROOMS);
+//        List chatRooms = new ArrayList<>(chatRoomMap.values());
+//        Collections.reverse(chatRooms);
+//        return chatRooms;
     }
 
     public ChatRoom findRoomById(String id){
-        return chatRoomMap.get(id);
+        return opsHashChatRoom.get(CHAT_ROOMS, id);
+//        return chatRoomMap.get(id);
     }
 
     public ChatRoom createChatRoom(String name){
         ChatRoom chatRoom = ChatRoom.create(name);
-        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
+//        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
         return chatRoom;
+    }
+
+    public void enterChatRoom(String roomId) {
+        ChannelTopic topic = topics.get(roomId);
+        if(topic == null){
+            topic = new ChannelTopic(roomId);
+        }
+        redisMessageListenerContainer.addMessageListener(redisSubscriber, topic);
+        topics.put(roomId, topic);
+    }
+
+    public ChannelTopic getTopic(String roomId){
+        return topics.get(roomId);
     }
 }
--- src/main/java/com/websocket/chat/web/ChatController.java
+++ src/main/java/com/websocket/chat/web/ChatController.java
@@ -2,6 +2,8 @@ package com.websocket.chat.web;
 
 import com.websocket.chat.domain.message.ChatMessage;
 import com.websocket.chat.domain.room.ChatRoom;
+import com.websocket.chat.domain.room.ChatRoomRepository;
+import com.websocket.chat.service.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -14,14 +16,19 @@ import java.util.List;
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
+
+//    private final SimpMessageSendingOperations messagingTemplate;
 
     @MessageMapping("/chat/message")
     public void message(ChatMessage message){
         if(ChatMessage.MessageType.ENTER.equals(message.getType())){
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender()+"님이 입장하셨습니다.");
         }
-        messagingTemplate.convertAndSend("/sub/chat/room/"+message.getRoomId(), message);
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
+//        messagingTemplate.convertAndSend("/sub/chat/room/"+message.getRoomId(), message);
     }
 
 //    @PostMapping
