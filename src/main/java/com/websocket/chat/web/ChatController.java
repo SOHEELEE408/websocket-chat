b343e9d21951d113728df4b3641762dce1b24981
minseok <minseok7407@naver.com>
2022-01-14 11:18:35 +0900

    메시지 타입 JOIN 추가, WebSocket으로 들어오는 메시지 발행 처리 컨트롤러, 채팅 화면 구성을 위한 Controller 생성

--- src/main/java/com/websocket/chat/web/ChatController.java
+++ src/main/java/com/websocket/chat/web/ChatController.java
@@ -1,26 +1,36 @@
 package com.websocket.chat.web;
 
+import com.websocket.chat.domain.message.ChatMessage;
 import com.websocket.chat.domain.room.ChatRoom;
-import com.websocket.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("chat")
+@Controller
 public class ChatController {
 
-    private final ChatService chatService;
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name){
-        return chatService.createRoom(name);
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message){
+        if(ChatMessage.MessageType.JOIN.equals(message.getType())){
+            message.setMessage(message.getSender()+"님이 입장하셨습니다.");
+        }
+        messagingTemplate.convertAndSend("/sub/chat/room/"+message.getRoomId(), message);
     }
 
-    @GetMapping
-    public List<ChatRoom> findAllRooms(){
-        return chatService.findAllRooms();
-    }
+//    @PostMapping
+//    public ChatRoom createRoom(@RequestParam String name){
+//        return chatService.createRoom(name);
+//    }
+//
+//    @GetMapping
+//    public List<ChatRoom> findAllRooms(){
+//        return chatService.findAllRooms();
+//    }
 }
